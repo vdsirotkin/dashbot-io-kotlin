7c44ea403f1b8d95f3a9990e156dadde7fa01a22
Vitaliy Sirotkin <vdsirotkin@dev.vtb>
2020-11-25 22:17:34 +0300

    fix dto

--- src/main/kotlin/com/vdsirotkin/dashbot/dto/TrackRequest.kt
+++ src/main/kotlin/com/vdsirotkin/dashbot/dto/TrackRequest.kt
@@ -1,10 +1,12 @@
 package com.vdsirotkin.dashbot.dto
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.vdsirotkin.dashbot.dto.track.Button
 import com.vdsirotkin.dashbot.dto.track.Image
 import com.vdsirotkin.dashbot.dto.track.Intent
 import com.vdsirotkin.dashbot.dto.track.Postback
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class TrackRequest(
     val text: String,
     val userId: String,
--- src/main/kotlin/com/vdsirotkin/dashbot/dto/track/Intent.kt
+++ src/main/kotlin/com/vdsirotkin/dashbot/dto/track/Intent.kt
@@ -2,6 +2,6 @@ package com.vdsirotkin.dashbot.dto.track
 
 data class Intent(
     val name: String,
-    val inputs: List<Input>,
-    val confidence: Float
+    val inputs: List<Input>?,
+    val confidence: Float?
 )
